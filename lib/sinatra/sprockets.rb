85fd3af041629bbe1e5129f7947880d12f2081a7
Andrew Bloom <andrew@andrewkbloom.com>
2012-03-18 22:30:03 -0500

    added some missing requires

--- lib/sinatra/sprockets.rb
+++ lib/sinatra/sprockets.rb
@@ -1,3 +1,7 @@
+require 'sprockets'
+require 'closure'
+require 'yui/compressor'
+
 require 'sinatra/sprockets/configuration'
 require 'sinatra/sprockets/asset_paths'
 require 'sinatra/sprockets/helpers'
@@ -8,13 +12,13 @@ module Sinatra
     def self.configure(&block)
       block.call(config)
       raise ArgumentError, "Cannot initialize Sprockets Environment without an app reference" if config.app.nil?
-      
+
       @environment = ::Sprockets::Environment.new(config.app.root)
-      
+
       config.paths.each do |path|
         environment.append_path(File.join(config.app.root, path))
       end
-      
+
       if config.compress_assets?
         environment.js_compressor = config.js_compressor || Closure::Compiler.new
         environment.css_compressor = config.css_compressor || YUI::CssCompressor.new
@@ -22,7 +26,7 @@ module Sinatra
         environment.js_compressor = false
         environment.css_compressor = false
       end
-      
+
       if config.manifest_path
         path = File.join(config.app.root, config.manifest_path, "manifest.yml")
       else
@@ -34,16 +38,16 @@ module Sinatra
           config.digests[path] = value
         end
       end
-      
+
       environment.context_class.instance_eval do
         include Helpers
       end
     end
-    
+
     def self.config
       @config ||= Configuration.new
     end
-    
+
     def self.environment
       @environment
     end
